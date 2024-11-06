d70e1f6c35b0fb055db6d11bb2594e4905071e36
Bobby Lashway <rola4910@colorado.edu>
2024-11-06 13:38:18 -0700

    poop

--- ProjectSourceCode/src/init_data/create.sql
+++ ProjectSourceCode/src/init_data/create.sql
@@ -3,10 +3,16 @@ CREATE TABLE playlists (
     name VARCHAR(100) NOT NULL,
     playlist_id VARCHAR(32) NOT NULL, -- used for API calls
     public BOOLEAN NOT NULL,
-
 );
 
 CREATE TABLE playlist_songs (
-    id SERIAL FOREIGN KEY,
+    id SERIAL PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
+    duration INTEGER NOT NULL,
+    artist VARCHAR(100) NOT NULL,
+    song_id VARCHAR(32) NOT NULL, -- used for API calls
+    album_name VARCHAR(100) NOT NULL,
+    album_release VARCHAR(100) NOT NULL,
+    added_at VARCHAR(100),
+    popularity INTEGER NOT NULL,
 )
