f67f973bb5c546318c1a1bd37bc608c8dd883006
Parker <pjsecrest@comcast.net>
2024-11-08 19:36:02 -0700

    formatting index.js auth code

--- ProjectSourceCode/src/index.js
+++ ProjectSourceCode/src/index.js
@@ -15,6 +15,12 @@ const axios = require('axios'); // To make HTTP requests from our server. We'll 
 const crypto = require('crypto'); // Node.js crypto module
 const querystring = require('querystring');
 
+
+
+// *****************************************************
+// <!-- Section 1 : Import Dependencies -->
+// *****************************************************
+
 // API AUTHORIZATION
 const generateRandomString = (length) => {
 	const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
@@ -26,84 +32,7 @@ const generateRandomString = (length) => {
 const clientId = "c25f72fe66174e8ab75756ddc591301f";
 const redirectUri = "http://localhost:3000/callback";
 const scope = 'user-read-private user-read-email';
-const clientSecret = '07e098d9c6d7421494042196d2b322fd';
 
-// temp login route for Spotify authorization
-app.get('/login2', function (req, res) {
-
-	var state = generateRandomString(16);
-
-	res.redirect('https://accounts.spotify.com/authorize?' +
-		querystring.stringify({
-			response_type: 'code',
-			client_id: clientId,
-			scope: scope,
-			redirect_uri: redirectUri,
-			state: state
-		}));
-});
-
-// callback function
-app.get('/callback', function (req, res) {
-
-	var code = req.query.code || null;
-	var state = req.query.state || null;
-
-	if (state === null) {
-		res.redirect('/login' +
-			querystring.stringify({
-				error: 'state_mismatch'
-			}));
-	} else {
-		var authOptions = {
-			url: 'https://accounts.spotify.com/api/token',
-			form: {
-				code: code,
-				redirect_uri: redirectUri,
-				grant_type: 'authorization_code'
-			},
-			headers: {
-				'content-type': 'application/x-www-form-urlencoded',
-				'Authorization': 'Basic ' + (new Buffer.from(clientId + ':' + clientSecret).toString('base64'))
-			},
-			json: true
-		};
-
-		// exchange authorization code for access token
-		axios.post(authOptions.url, authOptions.form, { headers: authOptions.headers })
-			.then(response => {
-				res.send(response.data);
-			})
-			.catch(error => {
-				res.send(error.message);
-			});
-	}
-});
-
-// TEST QUERY AGAINST SPOTIFY API - FETCH USER PLAYLISTS
-app.get('/getUserPlaylists', async (req, res) => {
-	// Replace {user_id} with the actual user ID you want to fetch playlists for
-	// TODO dynamically fetch userId
-	const userId = 'pisecrest';
-
-	// Assume accessToken is obtained during the authorization flow and stored in a variable
-	// fetch accessToken dynamically
-	const accessToken = 'BQCeFEY-jcaOayAWNM4sIfJ3hMVMx3vUyAw7tdG0IvrOSs3Ff_fa9BVS7YXNO4XFUXgfoUikYUBOvGRIZF8jyBAyTzY2WNKb6fVin8xHLQ9R4B92rkap6bOS3dGbM4zf-pGiN-A3vAhJ_cd6NSc4DCzKFVn6DWKzOwjNm8VAV9zA8hmj3aKCluG5NFfCmhBHLfVrG7rk';
-
-	const options = {
-		headers: {
-			'Authorization': `Bearer ${accessToken}`
-		}
-	};
-
-	try {
-		const response = await axios.get(`https://api.spotify.com/v1/users/${userId}/playlists`, options);
-		res.send(response.data);
-	} catch (error) {
-		console.error(error);
-		res.status(error.response.status).send(error.response.data);
-	}
-});
 
 // *****************************************************
 // <!-- Section 2 : Connect to DB -->
@@ -191,6 +120,82 @@ app.get('/logout', (req, res) => {
 	res.redirect('/login');
 });
 
+// temp login route for Spotify authorization
+app.get('/login2', function (req, res) {
+
+	var state = generateRandomString(16);
+
+	res.redirect('https://accounts.spotify.com/authorize?' +
+		querystring.stringify({
+			response_type: 'code',
+			client_id: clientId,
+			scope: scope,
+			redirect_uri: redirectUri,
+			state: state
+		}));
+});
+
+// callback function for auth
+app.get('/callback', function (req, res) {
+
+	var code = req.query.code || null;
+	var state = req.query.state || null;
+
+	if (state === null) {
+		res.redirect('/login' +
+			querystring.stringify({
+				error: 'state_mismatch'
+			}));
+	} else {
+		var authOptions = {
+			url: 'https://accounts.spotify.com/api/token',
+			form: {
+				code: code,
+				redirect_uri: redirectUri,
+				grant_type: 'authorization_code'
+			},
+			headers: {
+				'content-type': 'application/x-www-form-urlencoded',
+				'Authorization': 'Basic ' + (new Buffer.from(clientId + ':' + clientSecret).toString('base64'))
+			},
+			json: true
+		};
+
+		// exchange authorization code for access token
+		axios.post(authOptions.url, authOptions.form, { headers: authOptions.headers })
+			.then(response => {
+				res.send(response.data);
+			})
+			.catch(error => {
+				res.send(error.message);
+			});
+	}
+});
+
+// TEST QUERY AGAINST SPOTIFY API - FETCH USER PLAYLISTS
+app.get('/getUserPlaylists', async (req, res) => {
+	// Replace {user_id} with the actual user ID you want to fetch playlists for
+	// TODO dynamically fetch userId
+	const userId = 'pisecrest';
+
+	// Assume accessToken is obtained during the authorization flow and stored in a variable
+	// fetch accessToken dynamically
+	const accessToken = 'BQCeFEY-jcaOayAWNM4sIfJ3hMVMx3vUyAw7tdG0IvrOSs3Ff_fa9BVS7YXNO4XFUXgfoUikYUBOvGRIZF8jyBAyTzY2WNKb6fVin8xHLQ9R4B92rkap6bOS3dGbM4zf-pGiN-A3vAhJ_cd6NSc4DCzKFVn6DWKzOwjNm8VAV9zA8hmj3aKCluG5NFfCmhBHLfVrG7rk';
+
+	const options = {
+		headers: {
+			'Authorization': `Bearer ${accessToken}`
+		}
+	};
+
+	try {
+		const response = await axios.get(`https://api.spotify.com/v1/users/${userId}/playlists`, options);
+		res.send(response.data);
+	} catch (error) {
+		console.error(error);
+		res.status(error.response.status).send(error.response.data);
+	}
+});
 
 // *****************************************************
 // <!-- Section 5 : Start Server-->
