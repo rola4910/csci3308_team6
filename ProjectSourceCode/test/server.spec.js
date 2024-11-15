8bd21f5c15ef0039b55ec661aea8e15cac7fe787
Mary Ella Rinzler <mari4461@colorado.edu>
2024-11-14 21:48:23 -0500

    working on test cases

--- ProjectSourceCode/test/server.spec.js
+++ ProjectSourceCode/test/server.spec.js
@@ -101,3 +101,44 @@ describe('Testing Add User API', () => {
 //       });
 //   });
 // });
+
+
+
+
+describe('Testing Add User API', () => {
+  it('positive : /getUserPlaylists', done => {
+    chai
+      .request(server)
+      .get('/getUserPlaylists')
+      .end((err, res) => {
+        expect(res).to.have.status(200);
+        done();
+      });
+  });
+  it('Negative : /getUserPlaylists. Checking invalid name', done => {
+    chai
+      .request(server)
+      .get('/getUserPlaylists')
+      .end((err, res) => {
+        expect(res).to.have.status(401);
+        done();
+      });
+  });
+});
+
+
+
+describe('Testing Render', () => {
+  // Sample test case given to test /test endpoint.
+  it('test "/login" route should render with an html response', done => {
+    chai
+      .request(server)
+      .get('/login') // for reference, see lab 8's login route (/login) which renders home.hbs
+      .end((err, res) => {
+        res.should.have.status(200); // Expecting a success status code
+        res.should.be.html; // Expecting a HTML response
+        done();
+      });
+  });
+});
+
