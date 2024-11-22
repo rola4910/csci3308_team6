645f12620a9afe9c08909cac64c0cff60fbc37d3
Bobby Lashway <rola4910@colorado.edu>
2024-11-22 11:34:19 -0700

    who knows

--- ProjectSourceCode/src/index.js
+++ ProjectSourceCode/src/index.js
@@ -44,7 +44,7 @@ const hbs = handlebars.create({
 
 // database configuration
 const dbConfig = {
-	host: 'db', // the database server
+	host: 'dpg-ct0bpclumphs73f3qem0-a', // the database server
 	port: 5432, // the database port
 	database: process.env.POSTGRES_DB, // the database name
 	user: process.env.POSTGRES_USER, // the user account to connect with
